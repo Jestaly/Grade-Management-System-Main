c546ad2d4b08bcaf8a2d87aca33d66d41c8b30c9
Jzy <jestalycastillo15@gmail.com>
2024-11-16 20:32:40 +0800

    added admin form and connects login to db

---
+++ GradeManagementSystem/DatabaseConnector.vb
@@ -0,0 +1,10 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class DatabaseConnector
+    Public command As New MySqlCommand
+    Public dt As New DataTable
+    Public da As New MySqlDataAdapter
+    Public query As String
+    Public connect As New MySqlConnection("server=localhost;port=3307;user=root;password=joseph;database=student")
+
+
+End Class
--- GradeManagementSystem/LoginForm.vb
+++ GradeManagementSystem/LoginForm.vb
@@ -1,5 +1,35 @@
-﻿Public Class loginForm
+﻿Imports MySql.Data.MySqlClient
+
+Public Class loginForm
+    Public connector As New DatabaseConnector
     Private Sub loginButton_Click(sender As Object, e As EventArgs) Handles loginButton.Click
-        MessageBox.Show("Logged in!")
+        If (accountExist()) Then
+            MessageBox.Show("Logged in!")
+            Return
+        End If
+        MessageBox.Show("Account doesn't exists!")
     End Sub
+
+    Public Function accountExist() As Boolean
+        Try
+            connector.connect.Open()
+            connector.query = "SELECT COUNT(*) FROM login_info WHERE uname = ?username AND password = ?password;"
+            connector.command.Connection = connector.connect
+            connector.command.CommandText = connector.query
+            connector.command.Parameters.Clear()
+            connector.command.Parameters.AddWithValue("?username", usernameTextBox.Text)
+            connector.command.Parameters.AddWithValue("?password", passwordTextBox.Text)
+            Dim count As Integer = Convert.ToInt32(connector.command.ExecuteScalar())
+            If (count > 0) Then
+                connector.connect.Close()
+                Return True
+            End If
+        Catch ex As MySqlException
+            MessageBox.Show("Database Error")
+            Return False
+        End Try
+        connector.connect.Close()
+        Return False
+
+    End Function
 End Class
