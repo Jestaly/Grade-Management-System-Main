ab06be644e2215c0b74c1942fa4ed1fc4ac751da
Jzy <jestalycastillo15@gmail.com>
2024-11-18 12:34:51 +0800

    renamed da to dataAdapter and dt to dataTable

--- GradeManagementSystem/DatabaseFolder/DatabaseConnector.vb
+++ GradeManagementSystem/DatabaseFolder/DatabaseConnector.vb
@@ -1,8 +1,8 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class DatabaseConnector
     Public command As New MySqlCommand
-    Public dt As New DataTable
-    Public da As New MySqlDataAdapter
+    Public dataTable As New DataTable
+    Public dataAdapter As New MySqlDataAdapter
     Public query As String
     Public connect As New MySqlConnection("server=localhost;port=3307;user=root;password=joseph;database=grade_management_system")
 End Class
