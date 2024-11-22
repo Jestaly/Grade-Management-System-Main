0aac122398f3094bb83d7667c9ad592bc0611e99
Jzy <jestalycastillo15@gmail.com>
2024-11-22 14:13:15 +0800

    fixed ID diplay and added db for const count

--- GradeManagementSystem/AdminFolder/AddProfessorAccount.vb
+++ GradeManagementSystem/AdminFolder/AddProfessorAccount.vb
@@ -1,60 +1,28 @@
-﻿Imports System.Security.Permissions
-Imports MySql.Data.MySqlClient
+﻿Imports MySql.Data.MySqlClient
 
 Public Class AddProfessorAccount
     Private connector As New DatabaseConnector
     Private Sub addProfessorButton_Click(sender As Object, e As EventArgs) Handles addProfessorButton.Click
-
-        professorIDLabel.Text = getProfID()
-        Dim count As Integer
-        Try
-            connector.connect.Open()
-            connector.query = "SELECT COUNT(*) FROM faculty;"
-            connector.command.Connection = connector.connect
-            connector.command.CommandText = connector.query
-
-            count = Convert.ToInt32(connector.command.ExecuteScalar())
-            If (count = 0) Then
-
-            End If
-            connector.connect.Close()
-        Catch ex As MySqlException
-            connector.connect.Close()
-            MessageBox.Show("Database Error")
-        End Try
-
         Try
             connector.connect.Open()
             connector.query = "INSERT INTO faculty VALUES ('" & getProfID() & "','" & lastnameTextBox.Text & "','" & firstnameTextBox.Text & "','" & middlenameTextBox.Text & "','" & emailTextBox.Text & "');"
             connector.command.Connection = connector.connect
             connector.command.CommandText = connector.query
             connector.command.ExecuteNonQuery()
+            MessageBox.Show("Added Succesfully!")
             connector.connect.Close()
         Catch ex As MySqlException
             connector.connect.Close()
             MessageBox.Show("Database Error")
         End Try
     End Sub
-
     Private Function getProfID() As String
-        Dim idInt As Integer
-        'Try
-        'connector.connect.Open()
-
-        'connector.connect.Close()
-        'Catch ex As MySqlException
-        'connector.connect.Close()
-        'MessageBox.Show("Database Error at idIncrementer")
-        'End Try
-
-        idInt = 20000000
-        Dim year As String = DateAndTime.Now.Year
-        Dim idYearStr = year.Substring(2, 4)
-        Dim idYearInt = Integer.Parse(idYearStr)
-        idYearInt = idYearInt * 100000
-        idInt += idYearInt
-
-
-        Return ""
+        Dim profID = professorIDLabel.Text.Replace("-", "")
+        Return profID
     End Function
+
+    Private Sub AddProfessorAccount_Closing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        e.Cancel = True
+        Me.Visible = False
+    End Sub
 End Class
--- GradeManagementSystem/AdminFolder/ManageProfessorAdmin.vb
+++ GradeManagementSystem/AdminFolder/ManageProfessorAdmin.vb
@@ -3,7 +3,8 @@ Imports MySql.Data.MySqlClient
 
 Public Class ManageProfessorAdmin
     Private connector As New DatabaseConnector
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles refreshButton.Click
+    Private addProfessorAccount As New AddProfessorAccount
+    Private Sub refreshButton_Click(sender As Object, e As EventArgs) Handles refreshButton.Click
         Try
             connector.connect.Open()
             connector.dataTable.Clear()
@@ -21,6 +22,51 @@ Public Class ManageProfessorAdmin
     End Sub
 
     Private Sub addProfessorButton_Click(sender As Object, e As EventArgs) Handles addProfessorButton.Click
+        Try
+            connector.connect.Open()
+            connector.query = "SELECT count FROM pcount_history WHERE count "
+            connector.command.Connection = connector.connect
+            connector.command.CommandText = connector.query
+            Dim pCount As Integer = connector.command.ExecuteScalar
+            connector.connect.Close()
+        Catch ex As MySqlException
+            connector.connect.Close()
+            MessageBox.Show("Database Error")
+        End Try
+        Dim profSubString = Integer.Parse(addProfessorAccount.professorIDLabel.Text.Substring(5, 5))
+        MessageBox.Show(profSubString)
 
+        addProfessorAccount.professorIDLabel.Text = getProfNum() & "-" & getProfYear() & "-" & getZeros(profSubString) & (profSubString + 1)
+        addProfessorAccount.Visible = True
     End Sub
+
+    Private Function getProfNum() As String
+        Dim idInt As Integer
+        idInt = 2
+        Dim profNum As String = idInt
+
+        Return profNum
+    End Function
+    Private Function getProfYear() As Integer
+        Dim profYear As String = DateAndTime.Now.Year
+        profYear = profYear.Chars(2) & profYear.Chars(3)
+
+        Return profYear
+    End Function
+    Private Function getZeros(profSubString As Integer) As String
+        Dim zeros As String = "00000"
+        Select Case profSubString.ToString.Length
+            Case 1
+                zeros = "0000"
+            Case 2
+                zeros = "000"
+            Case 3
+                zeros = "00"
+            Case 4
+                zeros = "0"
+            Case 5
+                zeros = ""
+        End Select
+        Return zeros
+    End Function
 End Class
