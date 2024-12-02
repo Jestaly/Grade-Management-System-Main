098e69969a2e457d2a93291b9781b31053d6eb55
Jzy <jestalycastillo15@gmail.com>
2024-12-02 12:21:28 +0800

    connected db to cloud

--- GradeManagementSystem/DatabaseFolder/DatabaseConnector.vb
+++ GradeManagementSystem/DatabaseFolder/DatabaseConnector.vb
@@ -4,6 +4,6 @@ Public Class DatabaseConnector
     Public dataTable As New DataTable
     Public dataAdapter As New MySqlDataAdapter
     Public query As String
-    Public connect As New MySqlConnection("server=localhost;port=3307;user=root;database=grade_management_system")
+    Public connect As New MySqlConnection("server=junction.proxy.rlwy.net;port=53279;user=root;database=railway;password=UqyPpiCjXZajgtFXekWpBeuaocTXTkkt")
     Public reader As MySqlDataReader
 End Class
